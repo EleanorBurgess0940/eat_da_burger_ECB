4e95b6db503b3c381287bc8803f06fca33e00408
Eleanor Burgess <Eleanor.C.Burgess0940@gmail.com>
2020-05-18 19:38:45 -0400

    added config folder and added schema to sql

--- db/schema.sql
+++ db/schema.sql
@@ -4,7 +4,7 @@ CREATE DATABASE burgersDB;
 USE burgersDB;
 
 CREATE TABLE burgers(
-    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    burger_name VARCHAR(50) NOT NULL
-    devoured BOOLEAN 
-)
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    burger_name VARCHAR(50) NOT NULL,
+    devoured BOOLEAN DEFAULT false
+);
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Hamburger');
+INSERT INTO burgers (burger_name) VALUES ('BLTburger');
+INSERT INTO burgers (burger_name) VALUES ('Peanut Butter and Jelly Burger');
