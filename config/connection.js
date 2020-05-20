1f7721b14755e5a982953c3f82f9e13e85133477
Eleanor Burgess <Eleanor.C.Burgess0940@gmail.com>
2020-05-20 13:11:49 -0400

    set up connection

--- config/connection.js
+++ config/connection.js
@@ -4,13 +4,17 @@ const util = require("util");
 var connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "ne14LEELA!",
+  password: "",
   port: "3306",
   database: "burgersDB",
 });
 
 connection.connect(function (err) {
-  if (err) throw err;
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
 });
 
 connection.query = util.promisify(connection.query);
