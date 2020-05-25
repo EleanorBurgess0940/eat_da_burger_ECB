5e88a13ea18be6f05debd47fb1973674491c1ba3
Eleanor Burgess <Eleanor.C.Burgess0940@gmail.com>
2020-05-25 18:05:17 -0400

    added code for the delete button functionality

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -34,4 +34,32 @@ $(function () {
       location.reload();
     });
   });
+
+  $(".change-remake").on("click", function (event) {
+    var id = $(this).data("id");
+
+    var newDevouredState = {
+      devoured: false,
+    };
+
+    // Send the PUT request.
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newDevouredState,
+    }).then(function () {
+      location.reload();
+    });
+  });
+  $(".delete-burger").on("click", function (event) {
+    var id = $(this).data("id");
+
+    // Send the DELETE request.
+    $.ajax("/api/burgers/" + id, {
+      type: "DELETE",
+    }).then(function () {
+      console.log("deleted cat", id);
+      // Reload the page to get the updated list
+      location.reload();
+    });
+  });
 });
